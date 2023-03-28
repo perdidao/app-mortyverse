a88903b50f0e49f8d272318843cea3465ebc0cbd
Lucas Almeida <lucasecalmeida@gmail.com>
2023-03-28 15:59:22 -0300

    Initial Commit

---
+++ Mortyverse/MortyverseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MortyverseApp.swift
+//  Mortyverse
+//
+//  Created by Lucas Almeida on 28/03/23.
+//
+
+import SwiftUI
+
+@main
+struct MortyverseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
